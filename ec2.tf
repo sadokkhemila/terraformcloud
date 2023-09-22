8db9bf01d89de862ddd4bf445089144d14ad3a23
sadokkhemila <60410473+sadokkhemila@users.noreply.github.com>
2023-09-22 11:53:43 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region= "us-east-1"
+}
+# creation instance ec2
+resource "aws_instance"  "myec2" {
+    ami = "ami-012cc038cc685a0d7"
+    instance_type = "t2.micro"
+    key_name= "terra"
+   
+    tags= {
+        Name= "terraf-ec2"
+    }
+# supprimer le EBS (voulume) en meme temps que ec2
+    root_block_device {
+    delete_on_termination = true
+  }
