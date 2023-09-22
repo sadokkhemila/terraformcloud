ab613858bfe89f2949baa5ffb784d788d85e4a2d
sadokkhemila <60410473+sadokkhemila@users.noreply.github.com>
2023-09-22 12:36:29 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,12 +6,8 @@ resource "aws_instance"  "myec2" {
     ami = "ami-012cc038cc685a0d7"
     instance_type = "t2.micro"
     key_name= "terra"
-   
     tags= {
         Name= "terraf-ec2"
     }
-# supprimer le EBS (voulume) en meme temps que ec2
-    root_block_device {
-    delete_on_termination = true
-  }
+
 }
